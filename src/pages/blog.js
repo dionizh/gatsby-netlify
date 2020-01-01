c5bc92ddd2ff259096998ff6820e24d7bccf70ab
Dioni Zhong <dioniys@yahoo.com>
2020-01-01 22:30:30 +0000

    don't use description, add thumbnail to graphql query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -74,7 +74,7 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            description
+            thumbnail
           }
         }
       }
